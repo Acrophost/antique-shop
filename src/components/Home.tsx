54185a0e28ba62a73a6f8d176902dd463c579649
acrophost <anna.piasecka133@gmail.com>
2020-08-12 18:07:16 +0200

    Added base for home module

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,11 @@
 import React, { ReactElement } from 'react';
 
-const Home = (): ReactElement => <div className="home">Page--home</div>;
+const Home = (): ReactElement => (
+    <div className="home">
+        <div className="home__title-container">
+            <h1 className="home__title">The Antique House</h1>
+        </div>
+    </div>
+);
 
 export default Home;
