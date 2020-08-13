1f626e387c495bcdcee29b4e3809e38a262b7065
acrophost <anna.piasecka133@gmail.com>
2020-08-13 16:02:18 +0200

    Made home module fully responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,15 @@
 import React, { ReactElement } from 'react';
+import HomeBackground from '../svgs/HomeBackground';
 
 const Home = (): ReactElement => (
     <div className="home">
         <div className="home__title-container">
+            <svg className="home__accent-line" height="100" width="700">
+                <line x1="0" y1="0" x2="800" y2="0" />
+            </svg>
             <h1 className="home__title">The Antique House</h1>
         </div>
+        <HomeBackground className="home__background-furniture" />
     </div>
 );
 
