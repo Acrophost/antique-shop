17a6b5c68f2e29659b7b67c7cd8d7b0b06d8b844
acrophost <anna.piasecka133@gmail.com>
2020-09-11 16:27:22 +0200

    Created gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,153 @@ import Contact from './components/Contact';
 
 function App(): ReactElement {
     const location = '/';
+    const img1 = require('./images/a1.jfif');
+    const img2 = require('./images/a2.jfif');
+    const img3 = require('./images/a3.jfif');
+    const img4 = require('./images/a5.jfif');
+    const img5 = require('./images/a9.jfif');
+    const antiques = [
+        {
+            name: 'Lamp',
+            description:
+                'Donec tempus dolor ante, id lacinia sem tempor sit amet. Quisque at diam tortor. Vestibulum ligula tortor, bibendum luctus iaculis quis, facilisis id enim. Curabitur libero enim, rhoncus id elementum vel, interdum non elit. Sed lacinia facilisis ex, quis feugiat.',
+            image: String(img1),
+            alt: 'Vintage lamp',
+        },
+        {
+            name: 'Kettles',
+            description:
+                'Nullam eu sodales justo, vitae finibus nulla. Duis varius ultricies massa, a maximus elit. Vivamus dui erat, placerat et nisl id, tincidunt pharetra mi. Vestibulum efficitur, ante et aliquam accumsan, dui ex aliquet purus, nec aliquam lorem quam sed augue.',
+            image: String(img2),
+            alt: 'Two old, brass kettles',
+        },
+        {
+            name: 'Sewing machine',
+            description:
+                'Curabitur consectetur mollis volutpat. Curabitur at leo iaculis, pretium felis in, lobortis lectus. Nunc rutrum lacinia ligula, nec feugiat neque sagittis sed. Etiam vestibulum imperdiet imperdiet. In sed ligula bibendum, ultrices urna id, feugiat magna. In hac habitasse platea dictumst.',
+            image: String(img3),
+            alt: 'Black sewing machine with flower pattern',
+        },
+        {
+            name: 'Chair',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis gravida lacus, ac convallis nisi posuere a. Pellentesque efficitur lectus ex, porttitor luctus nunc eleifend sed. Phasellus consequat suscipit consectetur. Sed ornare non nulla et sollicitudin. Aenean sagittis sapien rhoncus.',
+            image: String(img4),
+            alt: 'An image of a vintage chair',
+        },
+        {
+            name: 'Figures of Holy Mary and Joseph',
+            description:
+                'Nam eu leo auctor, rhoncus quam id, accumsan mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam dignissim tempor ex, lobortis aliquet velit ultrices vel. Vivamus sollicitudin sed dui at tempus. Praesent volutpat at metus at egestas.',
+            image: String(img5),
+            alt: 'Two figures made of black stone',
+        },
+        {
+            name: 'Chair',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis gravida lacus, ac convallis nisi posuere a. Pellentesque efficitur lectus ex, porttitor luctus nunc eleifend sed. Phasellus consequat suscipit consectetur. Sed ornare non nulla et sollicitudin. Aenean sagittis sapien rhoncus.',
+            image: String(img4),
+            alt: 'An image of a vintage chair',
+        },
+        {
+            name: 'Figures of Holy Mary and Joseph',
+            description:
+                'Nam eu leo auctor, rhoncus quam id, accumsan mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam dignissim tempor ex, lobortis aliquet velit ultrices vel. Vivamus sollicitudin sed dui at tempus. Praesent volutpat at metus at egestas.',
+            image: String(img5),
+            alt: 'Two figures made of black stone',
+        },
+        {
+            name: 'Lamp',
+            description:
+                'Donec tempus dolor ante, id lacinia sem tempor sit amet. Quisque at diam tortor. Vestibulum ligula tortor, bibendum luctus iaculis quis, facilisis id enim. Curabitur libero enim, rhoncus id elementum vel, interdum non elit. Sed lacinia facilisis ex, quis feugiat.',
+            image: String(img1),
+            alt: 'Vintage lamp',
+        },
+        {
+            name: 'Kettles',
+            description:
+                'Nullam eu sodales justo, vitae finibus nulla. Duis varius ultricies massa, a maximus elit. Vivamus dui erat, placerat et nisl id, tincidunt pharetra mi. Vestibulum efficitur, ante et aliquam accumsan, dui ex aliquet purus, nec aliquam lorem quam sed augue.',
+            image: String(img2),
+            alt: 'Two old, brass kettles',
+        },
+        {
+            name: 'Sewing machine',
+            description:
+                'Curabitur consectetur mollis volutpat. Curabitur at leo iaculis, pretium felis in, lobortis lectus. Nunc rutrum lacinia ligula, nec feugiat neque sagittis sed. Etiam vestibulum imperdiet imperdiet. In sed ligula bibendum, ultrices urna id, feugiat magna. In hac habitasse platea dictumst.',
+            image: String(img3),
+            alt: 'Black sewing machine with flower pattern',
+        },
+        {
+            name: 'Lamp',
+            description:
+                'Donec tempus dolor ante, id lacinia sem tempor sit amet. Quisque at diam tortor. Vestibulum ligula tortor, bibendum luctus iaculis quis, facilisis id enim. Curabitur libero enim, rhoncus id elementum vel, interdum non elit. Sed lacinia facilisis ex, quis feugiat.',
+            image: String(img1),
+            alt: 'Vintage lamp',
+        },
+        {
+            name: 'Kettles',
+            description:
+                'Nullam eu sodales justo, vitae finibus nulla. Duis varius ultricies massa, a maximus elit. Vivamus dui erat, placerat et nisl id, tincidunt pharetra mi. Vestibulum efficitur, ante et aliquam accumsan, dui ex aliquet purus, nec aliquam lorem quam sed augue.',
+            image: String(img2),
+            alt: 'Two old, brass kettles',
+        },
+        {
+            name: 'Sewing machine',
+            description:
+                'Curabitur consectetur mollis volutpat. Curabitur at leo iaculis, pretium felis in, lobortis lectus. Nunc rutrum lacinia ligula, nec feugiat neque sagittis sed. Etiam vestibulum imperdiet imperdiet. In sed ligula bibendum, ultrices urna id, feugiat magna. In hac habitasse platea dictumst.',
+            image: String(img3),
+            alt: 'Black sewing machine with flower pattern',
+        },
+        {
+            name: 'Chair',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis gravida lacus, ac convallis nisi posuere a. Pellentesque efficitur lectus ex, porttitor luctus nunc eleifend sed. Phasellus consequat suscipit consectetur. Sed ornare non nulla et sollicitudin. Aenean sagittis sapien rhoncus.',
+            image: String(img4),
+            alt: 'An image of a vintage chair',
+        },
+        {
+            name: 'Figures of Holy Mary and Joseph',
+            description:
+                'Nam eu leo auctor, rhoncus quam id, accumsan mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam dignissim tempor ex, lobortis aliquet velit ultrices vel. Vivamus sollicitudin sed dui at tempus. Praesent volutpat at metus at egestas.',
+            image: String(img5),
+            alt: 'Two figures made of black stone',
+        },
+        {
+            name: 'Chair',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis gravida lacus, ac convallis nisi posuere a. Pellentesque efficitur lectus ex, porttitor luctus nunc eleifend sed. Phasellus consequat suscipit consectetur. Sed ornare non nulla et sollicitudin. Aenean sagittis sapien rhoncus.',
+            image: String(img4),
+            alt: 'An image of a vintage chair',
+        },
+        {
+            name: 'Figures of Holy Mary and Joseph',
+            description:
+                'Nam eu leo auctor, rhoncus quam id, accumsan mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam dignissim tempor ex, lobortis aliquet velit ultrices vel. Vivamus sollicitudin sed dui at tempus. Praesent volutpat at metus at egestas.',
+            image: String(img5),
+            alt: 'Two figures made of black stone',
+        },
+        {
+            name: 'Lamp',
+            description:
+                'Donec tempus dolor ante, id lacinia sem tempor sit amet. Quisque at diam tortor. Vestibulum ligula tortor, bibendum luctus iaculis quis, facilisis id enim. Curabitur libero enim, rhoncus id elementum vel, interdum non elit. Sed lacinia facilisis ex, quis feugiat.',
+            image: String(img1),
+            alt: 'Vintage lamp',
+        },
+        {
+            name: 'Kettles',
+            description:
+                'Nullam eu sodales justo, vitae finibus nulla. Duis varius ultricies massa, a maximus elit. Vivamus dui erat, placerat et nisl id, tincidunt pharetra mi. Vestibulum efficitur, ante et aliquam accumsan, dui ex aliquet purus, nec aliquam lorem quam sed augue.',
+            image: String(img2),
+            alt: 'Two old, brass kettles',
+        },
+        {
+            name: 'Sewing machine',
+            description:
+                'Curabitur consectetur mollis volutpat. Curabitur at leo iaculis, pretium felis in, lobortis lectus. Nunc rutrum lacinia ligula, nec feugiat neque sagittis sed. Etiam vestibulum imperdiet imperdiet. In sed ligula bibendum, ultrices urna id, feugiat magna. In hac habitasse platea dictumst.',
+            image: String(img3),
+            alt: 'Black sewing machine with flower pattern',
+        },
+    ];
 
     return (
         <div className="App">
@@ -18,7 +165,7 @@ function App(): ReactElement {
             <Home />
             <About />
             <Sales />
-            <Gallery />
+            <Gallery antiques={antiques} />
             <Contact />
         </div>
     );
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,42 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 
-const Gallery = (): ReactElement => <div className="gallery">Page--gallery</div>;
+type Antique = { name: string; description: string; image: string; alt: string };
+
+type AntiqueProps = { antiques: Antique[] };
+
+const Gallery = (props: AntiqueProps): ReactElement => {
+    const [currentlyHovered, setCurrentHover] = useState(-1);
+
+    return (
+        <div className="gallery">
+            <h2 className="gallery__title">Our antiques</h2>
+            <svg className="gallery__accent-line" height="100" width="700">
+                <line x1="0" y1="0" x2="800" y2="0" />
+            </svg>
+            <div className="gallery__images-container">
+                {props.antiques.map((val, i) => {
+                    return (
+                        <button
+                            className="gallery__button"
+                            key={i}
+                            onMouseEnter={(): void => setCurrentHover(i)}
+                            onMouseLeave={(): void => setCurrentHover(-1)}
+                        >
+                            <img src={val.image} alt={val.alt} className="gallery__img" />
+                            {currentlyHovered === i && (
+                                <div className="gallery__button-overlay">
+                                    <div className="gallery__button-text">
+                                        <h3>{val.name}</h3>
+                                        <p>See details</p>
+                                    </div>
+                                </div>
+                            )}
+                        </button>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
 
 export default Gallery;
