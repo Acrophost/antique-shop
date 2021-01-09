8f1d0707a602831eb79570878d8cc4b2de7d2c7f
acrophost <anna.piasecka133@gmail.com>
2021-01-09 20:29:46 +0100

    Updates

---
+++ precache-manifest.04b6e209d2e56e4957d6df03d07b4bb2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7368bf48ecf5f94a6af81bc7afac3a",
+    "url": "/antique-shop/index.html"
+  },
+  {
+    "revision": "b607a572a7436a67b3c3",
+    "url": "/antique-shop/static/css/main.0bae2dd9.chunk.css"
+  },
+  {
+    "revision": "b695ff6295a00358b904",
+    "url": "/antique-shop/static/js/2.b3fb48fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/antique-shop/static/js/2.b3fb48fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b607a572a7436a67b3c3",
+    "url": "/antique-shop/static/js/main.ab70ec41.chunk.js"
+  },
+  {
+    "revision": "ba54fe1f83851fa7b149",
+    "url": "/antique-shop/static/js/runtime-main.b4869a2a.js"
+  },
+  {
+    "revision": "6ad5977af7e9526904a2f172cb487d15",
+    "url": "/antique-shop/static/media/a1.6ad5977a.jfif"
+  },
+  {
+    "revision": "103f857995a97e292f40357862119838",
+    "url": "/antique-shop/static/media/a2.103f8579.jfif"
+  },
+  {
+    "revision": "440de8c869761a6ca0d3faa2ee48a2fd",
+    "url": "/antique-shop/static/media/a3.440de8c8.jfif"
+  },
+  {
+    "revision": "e0d6ad972655fb0599e100f94d377ee2",
+    "url": "/antique-shop/static/media/a5.e0d6ad97.jfif"
+  },
+  {
+    "revision": "beb63575f9f571d563b8e8beb4a09466",
+    "url": "/antique-shop/static/media/a9.beb63575.jfif"
+  },
+  {
+    "revision": "1600d5540bfb38197edb74ca9114a4a8",
+    "url": "/antique-shop/static/media/home-bg2.1600d554.jfif"
+  },
+  {
+    "revision": "e21e05ed709c4fb5aa144b086c8776e6",
+    "url": "/antique-shop/static/media/sale-bg.e21e05ed.jfif"
+  }
+]);
